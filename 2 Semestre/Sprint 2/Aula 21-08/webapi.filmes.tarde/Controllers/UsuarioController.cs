07559603caa4ac30c6cb82e39728357e73cc2489
Pedro <pedrohbedin@gmail.com>
2023-09-01 17:14:29 -0300

    implementação finalizada

--- 2 Semestre/Sprint 2/Aula 21-08/webapi.filmes.tarde/Controllers/UsuarioController.cs
+++ 2 Semestre/Sprint 2/Aula 21-08/webapi.filmes.tarde/Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using webapi.filmes.tarde.Domains;
 using webapi.filmes.tarde.Interfaces;
 using webapi.filmes.tarde.Repositories;
@@ -20,18 +24,57 @@ namespace webapi.filmes.tarde.Controllers
 
         [HttpPost]
 
-        public IActionResult Login( string Email, string Senha)
+        public IActionResult Login(UsuarioDomain usuario)
         {
             try
             {
-               UsuarioDomain UsuarioAchado = _UsuarioRepository.Login(Email, Senha);
+               UsuarioDomain UsuarioAchado = _UsuarioRepository.Login(usuario.Email, usuario.Senha);
 
                 if (UsuarioAchado == null)
                 {
                     return NotFound("Email ou senha incorretos");
                 }
+                //Caso encontro o usuário buscado irá proseeguir para criação do token
 
-                return Ok(UsuarioAchado);
+                //1 Definição as informações que serão fornecidos no token
+
+                var claims = new[]
+                {
+                    //formato da Claim(tipo, valor)
+                    new Claim(JwtRegisteredClaimNames.Jti, UsuarioAchado.IdUsuario.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, UsuarioAchado.Email),
+                    new Claim(ClaimTypes.Role, UsuarioAchado.Permissao),
+                    new Claim("Claim personalizada", "Valor personalizado")
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("filmes-chave-autenticacao-webapi-dev"));
+
+                //Definir as credenciais do token(Header)
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken
+                (
+                    //Emissor do token
+                    issuer: "webapi.filmes.tarde",
+
+                    audience: "webapi.filmes.tarde",
+
+                    //Dados definidos nas claims 
+                    claims : claims,
+
+                    //tempo de expiração
+                    expires : DateTime.Now.AddMinutes(5),
+
+                    //credencias do token
+                    signingCredentials : creds
+                ) ;
+                //retornar o token 
+
+                return Ok(new 
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
 
             }
             catch (Exception error)
--- 2 Semestre/Sprint 2/Aula 21-08/webapi.filmes.tarde/Program.cs
+++ 2 Semestre/Sprint 2/Aula 21-08/webapi.filmes.tarde/Program.cs
@@ -1,45 +1,60 @@
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-//Vai adicionar o serviço de controllers
-builder.Services.AddControllers();
-//Adiciona o gerador do swagger 
-builder.Services.AddSwaggerGen(options =>
+internal class Program
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    private static void Main(string[] args)
     {
-        Version = "v1",
-        Title = "API filmes tarde",
-        Description = "API para gerenciamento de filmes - introdução a sprnt 2 - backend API",
-        Contact = new OpenApiContact
+        var builder = WebApplication.CreateBuilder(args);
+        //Vai adicionar o serviço de controllers
+        builder.Services.AddControllers();
+
+        //Adiciona serviço de autentificação
+        builder.Services.AddAuthentication(options =>
         {
-            Name = "Pedro Henrique",
-            Url = new Uri("https://github.com/Pedrohbedin/Pedrohbedin")
+            options.DefaultChallengeScheme = "JwtBearer";
+            options.DefaultAuthenticateScheme = "JwtBearer";
+        })
+
+        .AddJwtBearer(options => { });//Paramos aqui, continuar na segunda.
+        //Adiciona o gerador do swagger 
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "API filmes tarde",
+                Description = "API para gerenciamento de filmes - introdução a sprnt 2 - backend API",
+                Contact = new OpenApiContact
+                {
+                    Name = "Pedro Henrique",
+                    Url = new Uri("https://github.com/Pedrohbedin/Pedrohbedin")
+                }
+            });
+
+            // Configurar o Swagger para usar o arquivo XML gerado
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        var app = builder.Build();
+
+        //Habilite o middleware para atender ao documento JSON gerado e à interface do usuário do Swagger
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    });
 
-    // Configurar o Swagger para usar o arquivo XML gerado
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+        //Para atender à interface do usuário do Swagger na raiz do aplicativo
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
 
-var app = builder.Build();
-
-//Habilite o middleware para atender ao documento JSON gerado e à interface do usuário do Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        //Mapeia os controllers 
+        app.MapControllers();
+        app.Run();
+    }
 }
-
-//Para atender à interface do usuário do Swagger na raiz do aplicativo
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
-
-//Mapeia os controllers 
-app.MapControllers();
-app.Run();
