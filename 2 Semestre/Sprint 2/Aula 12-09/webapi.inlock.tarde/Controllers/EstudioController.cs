08cc777815267387e4d6d943142d220f1f38a14d
Pedro <pedrohbedin@gmail.com>
2023-09-12 17:23:48 -0300

    webapi iniciada, com listar estudios finalizado

---
+++ 2 Semestre/Sprint 2/Aula 12-09/webapi.inlock.tarde/Controllers/EstudioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.inlock.tarde.Interface;
+using webapi.inlock.tarde.Repositorys;
+
+namespace webapi.inlock.tarde.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EstudioController : ControllerBase
+    {
+        private IEstudioRepository _estudioRepository = new EstudioRepository();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_estudioRepository.Listar());
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error.Message);
+            }
+        }
+    }
+}
---
+++ 2 Semestre/Sprint 2/Aula 12-09/webapi.inlock.tarde/Interface/IEstudioRepository.cs
@@ -0,0 +1,18 @@
+﻿using webapi.inlock.tarde.Domains;
+
+namespace webapi.inlock.tarde.Interface
+{
+    public interface IEstudioRepository
+    {
+        List<Estudio> Listar();
+
+        Estudio BuscarPorId(Guid Id);
+
+        void Cadastrar(Estudio estudio);
+
+        void Atualizar(Guid Id, Estudio estudio);
+
+        void Deletar(Guid Id);
+        List<Estudio> ListarComJogos();
+    }
+}
