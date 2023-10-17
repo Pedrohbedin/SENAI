1e8a5b08c9c718259583e41965c8f165782d1537
Pedro <pedrohbedin@gmail.com>
2023-10-17 17:19:55 -0300

    projeto imc finalizado

--- 2 Semestre/Sprint 3/Projetos/imc15/js/script.js
+++ 2 Semestre/Sprint 3/Projetos/imc15/js/script.js
@@ -1,49 +1,42 @@
+let usuarios = [];
+let usuario
 function calcular(event) {
     event.preventDefault();
     let nome = document.getElementById("nome").value;
     let altura = parseFloat(document.getElementById("altura").value);
     let peso = parseFloat(document.getElementById("peso").value);
-    let IMC = (peso/(altura * altura));
+    let IMC = (peso / (altura * altura));
     let Situacao;
     let now = new Date();
 
-    if(isNaN(peso) || isNaN(altura) || nome.lenght > 2 ){
-        alert("Preencha todos os campos");
+    if (isNaN(peso) || isNaN(altura) || nome.lenght > 2) {
+        alert("Preencha todos os campos corretamente");
+        return;
     }
 
     if (IMC <= 17) {
         Situacao = "Muito abaixo do peso";
     }
     else if (IMC > 17 && IMC <= 18.49) {
-        Situacao = "Abaixo do peso"; 
+        Situacao = "Abaixo do peso";
     }
     else if (IMC >= 18.5 && IMC <= 24.99) {
-        Situacao = "Peso normal"; 
+        Situacao = "Peso normal";
     }
     else if (IMC >= 25 && IMC <= 29.99) {
-        Situacao = "Sobrepeso"; 
+        Situacao = "Sobrepeso";
     }
     else if (IMC >= 30 && IMC <= 34.99) {
-        Situacao = "Obesidade I"; 
+        Situacao = "Obesidade I";
     }
     else if (IMC >= 35 && IMC <= 39.99) {
-        Situacao = "Obesidade II"; 
-     }
+        Situacao = "Obesidade II";
+    }
     else if (IMC >= 40) {
-        Situacao = "Obesidade III"; 
+        Situacao = "Obesidade III";
     }
 
-    console.log(`
-    nome; ${nome}
-    altura; ${altura}
-    peso: ${peso}
-    IMC: ${IMC}
-    Sitação: ${Situacao}
-    Data: ${now}`);
-
-    let usuarios = [];
-
-    let usuario = {
+    usuario = {
         nome: nome,
         altura: altura.toFixed(2),
         peso: peso.toFixed(2),
@@ -54,20 +47,28 @@ function calcular(event) {
 
     usuarios.push(usuario);
 
-    document.getElementById('corpo-tabela').innerHTML += 
-    `
-    <tr>
-        <td data-cell="nome">${usuario.nome}</td>
-        <td data-cell="altura">${usuario.altura}</td>
-        <td data-cell="peso">${usuario.peso}</td>
-        <td data-cell="valor do IMC">${usuario.IMC}</td>
-        <td data-cell="classificação do IMC">${Situacao}</td>
-        <td data-cell="data de cadastro">${usuario.Data}</td>
-        </tr>
-    `
+    adicionarRegistros();
 }
 
-function deletarRegistros(event) {
+function deletarRegistros() {
     usuarios = [];
     document.getElementById('corpo-tabela').innerHTML = "";
+}
+
+function adicionarRegistros() {
+    let linhas = ""
+    usuarios.forEach(function (oPessoa) {
+        linhas +=
+            `
+        <tr>
+            <td>${oPessoa.nome}</td>
+            <td>${oPessoa.altura}</td>
+            <td>${oPessoa.peso}</td>
+            <td>${oPessoa.IMC}</td>
+            <td>${oPessoa.Situacao}</td>
+            <td>${oPessoa.Data}</td>
+        </tr>
+        `
+    });
+    document.getElementById('corpo-tabela').innerHTML = linhas;
 }
