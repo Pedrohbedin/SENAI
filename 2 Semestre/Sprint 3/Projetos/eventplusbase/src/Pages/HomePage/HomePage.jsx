a875e51341105218e53f99381150112f74ee764a
Pedro Henrique <pedrohbedin@gmail.com>
2023-11-08 17:17:05 -0300

    event+ atualizado

--- 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Pages/HomePage/HomePage.jsx
+++ 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Pages/HomePage/HomePage.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import MainContent from '../../Components/MainContent/MainContent';
-import Banner from '../../Components/Banner/Banner';
+import React from "react";
+import MainContent from "../../Components/MainContent/MainContent";
+import Banner from "../../Components/Banner/Banner";
+import VisionSection from "../../Components/VisionSection/VisionSection";
+import ContatctSection from "../../Components/ContactSection/ContactSection";
+import NextEvent from "../../Components/NextEvent/NextEvent";
+import Container from "../../Components/Container/Container";
+import Title from "../../Components/Title/Title";
 
-
+import "./HomePage.css";
 
 const HomePage = () => {
-    return (
-        <MainContent>
-            <Banner/>
-        </MainContent>
-    );
+  return (
+    <MainContent>
+      <Banner />
+      {/* Proximos eventos */}
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Próximos Eventos"} />
+          <div className="events-box">
+            {/* {NextEvent.map((e) => (
+              <NextEvent
+                key={e.idEvento}
+                idEvent={e.idEvento}
+                title={e.nomeEvento}
+                description={e.descricao}
+                eventDate={e.dataEvento}
+              />
+            ))} */}
+            <NextEvent
+              title={"Happy Hour Event"}
+              description={"qweqweqweqwe"}
+              eventDate={"29/09/23"}
+            />
+          </div>
+        </Container>
+      </section>
+      <VisionSection />
+      <ContatctSection />
+    </MainContent>
+  );
 };
 
 export default HomePage;
