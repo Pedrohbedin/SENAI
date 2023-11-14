d26e2507437edf1f0fbab81bf061fa0cbc0ceeb5
Pedro Henrique <pedrohbedin@gmail.com>
2023-11-14 17:10:14 -0300

    Cadastro de tipo eventos finalizada

--- 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Components/FormComponents/FormComponents.jsx
+++ 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Components/FormComponents/FormComponents.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./FormComponents.css";
 
-export const Input = ({ type, id, required, aditionalClass, name, placeholder, manipulationFunction }) => {
+export const Input = ({
+  type,
+  id,
+  required,
+  aditionalClass,
+  name,
+  placeholder,
+  manipulationFunction,
+  value
+}) => {
   return (
-    <input type={type} id={id} required={required} className={`input-component ${aditionalClass}`} placeholder={placeholder} onChange={manipulationFunction} autoComplete='off' />
+    <input
+      type={type}
+      id={id}
+      required={required}
+      className={`input-component ${aditionalClass}`}
+      placeholder={placeholder}
+      onChange={manipulationFunction}
+      autoComplete="off"
+      value={value}
+    />
+  );
+};
+
+export const Button = ({
+  texButton,
+  id,
+  name,
+  type,
+  aditionalClass,
+  manipulationFunction
+}) => {
+  return (
+    <button
+      type={type}
+      id={id}
+      name={name}
+      className={`button-component ${aditionalClass}`}
+      onClick={manipulationFunction}
+    >{texButton}</button>
   );
 };
--- 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Pages/TipoEventosPages/TipoEventosPage.jsx
+++ 2 Semestre/Sprint 3/Projetos/eventplusbase/src/Pages/TipoEventosPages/TipoEventosPage.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageIllustrator from "../../Components/ImageIllustrator/ImageIllustrator";
 import MainContent from "../../Components/MainContent/MainContent";
 import Title from "../../Components/Title/Title";
 import "./TipoEventosPage.css";
 import eventTypeImage from "../../assets/images/tipo-evento.svg";
 import Container from "../../Components/Container/Container";
-import { Input } from "../../Components/FormComponents/FormComponents";
+import { Input, Button } from "../../Components/FormComponents/FormComponents";
+import api from "../../Services/Services";
 
 const TipoEventosPage = () => {
+  const [frmEdit, setFrmEdit] = useState(false);
+  const [titulo, setTitulo] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (titulo.trim().length < 3) {
+      alert("O Título deve ter no míninmo 3 caracteres");
+      return;
+    }
+
+    try {
+      const retorno = await api.post("/TiposEvento", {titulo: titulo})
+      console.log(retorno.data);
+      setTitulo("")
+    }catch(error) {
+      console.log(error)
+    }
+  }
+
+  function handleUpdate() {
+    alert("Bora atualizar");
+  }
   return (
     <MainContent>
       <section className="cadastro-evento-section">
@@ -19,9 +43,35 @@ const TipoEventosPage = () => {
               color={""}
             />
             <ImageIllustrator alterText="" imageRender={eventTypeImage} />
-            <form action="">
-              <p>Componente de Formulário</p>
-                <Input type={"number"} required={"required"}/>
+            <form
+              className="ftipo-evento"
+              onSubmit={frmEdit ? handleUpdate : handleSubmit}
+            >
+              <p>
+                {!frmEdit ? (
+                  <>
+                    <Input
+                      type={"text"}
+                      id={"text"}
+                      name={"titulo"}
+                      placeholder={"Título"}
+                      required={"required"}
+                      value={titulo}
+                      manipulationFunction={(e) => {
+                        setTitulo(e.target.value);
+                      }}
+                    />
+                  </>
+                ) : (
+                  <p>Tela de edição</p>
+                )}
+              </p>
+              <Button
+                type={"submit"}
+                id={"cadastrar"}
+                name={"cadastrar"}
+                texButton={"Cadastrar"}
+              />
             </form>
           </div>
         </Container>
